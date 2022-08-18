1ca72a0f78914ee034d5c9a28c689375a7a6c0ad
BrunoRodriguesN <bruno_adeira@hotmail.com>
2022-08-18 19:29:48 -0300

    classe user dto

---
+++ src/main/java/br/com/bruno/api/domain/dto/UserDTO.java
@@ -0,0 +1,20 @@
+package br.com.bruno.api.domain.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class UserDTO {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+}
