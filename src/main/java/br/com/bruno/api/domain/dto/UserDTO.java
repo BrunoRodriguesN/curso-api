2fdb8b67e6ccc3a3e5c0226da37c78f195edb4ce
BrunoRodriguesN <bruno_adeira@hotmail.com>
2022-08-19 00:24:39 -0300

    fix bug

--- src/main/java/br/com/bruno/api/domain/dto/UserDTO.java
+++ src/main/java/br/com/bruno/api/domain/dto/UserDTO.java
@@ -4,11 +4,13 @@ package br.com.bruno.api.domain.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 
 public class UserDTO {
 
