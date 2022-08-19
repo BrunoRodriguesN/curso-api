1f0ff0fc44fd572f658877a174afce67cc2db044
BrunoRodriguesN <bruno_adeira@hotmail.com>
2022-08-19 00:19:22 -0300

    removendo anotações nao utilizadas

--- src/main/java/br/com/bruno/api/domain/dto/UserDTO.java
+++ src/main/java/br/com/bruno/api/domain/dto/UserDTO.java
@@ -4,12 +4,10 @@ package br.com.bruno.api.domain.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 
 public class UserDTO {
